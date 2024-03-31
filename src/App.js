fd0ce257d1eb50da2219f50bf401a1c089842971
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-03-31 22:40:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/signup'>
+          <AuthPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
